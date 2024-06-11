1b6148e16e0c9414f045da29cd95f2f0a549b1e4
Eliott Wantz <70651737+EliottWantz@users.noreply.github.com>
2024-06-11 18:37:26 -0400

    redirect when post created

--- app/Livewire/Posts/ListPosts.php
+++ app/Livewire/Posts/ListPosts.php
@@ -3,6 +3,9 @@
 namespace App\Livewire\Posts;
 
 use App\Models\Post;
+use Illuminate\Contracts\Pagination\LengthAwarePaginator;
+use Illuminate\Database\Eloquent\Collection;
+use Livewire\Attributes\On;
 use Livewire\Component;
 use Livewire\WithPagination;
 
@@ -10,10 +13,18 @@ class ListPosts extends Component
 {
     use WithPagination;
 
+    #[On('post-created')]
+    public function refresh()
+    {
+        $this->redirect(route('posts.index'));
+    }
+
     public function render()
     {
         return view('livewire.posts.list-posts', [
-            'posts' => Post::with('user')->paginate(10),
+            'posts' => Post::with('user')
+                ->latest()
+                ->paginate(10)
         ]);
     }
 }
